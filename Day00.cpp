85024de31a17b862f4b8d2a2b4a3d10552da7935
Shruti Kumari <shruti.25bce10766@vitbhopal.ac.in>
2026-06-11 00:04:52 +0530

    Add files via upload

---
+++ Day00.cpp
@@ -0,0 +1,25 @@
+//TwoSum 
+#include <iostream>
+#include <vector>
+using namespace std;
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector <int> ans;
+        int n = nums.size();
+        for (int i=0;i<n;i++){
+            for (int j=i+1; j<n;j++){
+                if (nums[i]+nums[j]==target){
+                    ans.push_back(i);
+                    ans.push_back(j);
+                    return ans;
+                }
+            }
+        }
+        return ans;
+    }
+int main(){
+    vector<int>nums={1,2,3,4,5,6,7,8};
+    int target = 9;
+    vector<int>ans=twoSum(nums,target);
+    cout<<ans[0]<<","<<ans[1]<<endl;
+    return 0;
+}
