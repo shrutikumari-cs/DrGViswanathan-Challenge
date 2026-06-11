8ffa010a5b93f839d9d348a2b578fefc615cf2bb
Shruti Kumari <shruti.25bce10766@vitbhopal.ac.in>
2026-06-12 01:47:04 +0530

    Add files via upload

---
+++ Day02.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+bool isValid(string s) {
+    stack<char> st;
+
+    for(char ch : s) {
+
+        if(ch == '(' || ch == '{' || ch == '[') {
+            st.push(ch);
+        }
+        else {
+
+            if(st.empty())
+                return false;
+
+            if(st.top() == '(' && ch == ')')
+                st.pop();
+
+            else if(st.top() == '{' && ch == '}')
+                st.pop();
+
+            else if(st.top() == '[' && ch == ']')
+                st.pop();
+
+            else
+                return false;
+        }
+    }
+
+    return st.empty();
+}
+
+int main() {
+
+    string s;
+
+    cout << "Enter brackets string: ";
+    cin >> s;
+
+    if(isValid(s))
+        cout << "Valid Parentheses";
+    else
+        cout << "Invalid Parentheses";
+
+    return 0;
+}
