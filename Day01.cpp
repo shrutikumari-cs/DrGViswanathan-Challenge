fe6766be2b103f4e35ba610a414e0e6bd3630bd4
Shruti Kumari <shruti.25bce10766@vitbhopal.ac.in>
2026-06-11 23:50:54 +0530

    Add files via upload

---
+++ Day01.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+bool isPalindrome(int n) {
+    int original = n;
+    int reversed = 0;
+
+    while(n > 0) {
+        int digit = n % 10;
+        reversed = reversed * 10 + digit;
+        n = n / 10;
+    }
+
+    return original == reversed;
+}
+
+int main() {
+    int num;
+    cout << "Enter number: ";
+    cin >> num;
+
+    if(isPalindrome(num))
+        cout << num << " is a palindrome";
+    else
+        cout << num << " is not a palindrome";
+
+    return 0;
+}
