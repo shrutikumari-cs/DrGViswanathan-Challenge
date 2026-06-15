b8ed0927a13208669622f7879a66e8a227367eed
Shruti Kumari <shrutikumarism226@gmail.com>
2026-06-15 21:33:50 +0530

    Add files via upload

---
+++ Day05.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int maxProfit(vector<int>& prices) {
+    int minPrice = prices[0];
+    int maxProfit = 0;
+
+    for (int i = 0; i < prices.size(); i++) {
+
+        if (minPrice > prices[i]) {
+            minPrice = prices[i];
+        }
+
+        int profit = prices[i] - minPrice;
+
+        maxProfit = max(maxProfit, profit);
+    }
+
+    return maxProfit;
+}
+
+int main() {
+
+    vector<int> prices = {7, 1, 5, 3, 6, 4};
+
+    cout << "Maximum Profit = "
+         << maxProfit(prices) << endl;
+
+    return 0;
+}
