df68237ea8e846505051752b4aa61bdf1a67638f
Shruti Kumari <shrutikumarism226@gmail.com>
2026-06-16 23:48:35 +0530

    Add files via upload

---
+++ Day06.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isAnagram(string s, string t) {
+
+    if (s.length() != t.length())
+        return false;
+
+    int freq[26] = {0};
+
+    for (char ch : s) {
+        freq[ch - 'a']++;
+    }
+
+    for (char ch : t) {
+        freq[ch - 'a']--;
+    }
+
+    for (int i = 0; i < 26; i++) {
+        if (freq[i] != 0)
+            return false;
+    }
+
+    return true;
+}
+
+int main() {
+
+    string s = "anagram";
+    string t = "nagaram";
+
+    if (isAnagram(s, t))
+        cout << "True" << endl;
+    else
+        cout << "False" << endl;
+
+    return 0;
+}
