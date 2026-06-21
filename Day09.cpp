8819bc7e0337b5f334b604fa46fa41872aa4ac80
Shruti Kumari <shrutikumarism226@gmail.com>
2026-06-21 21:00:04 +0530

    Add files via upload

---
+++ Day09.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int findNumbers(vector<int>& nums) {
+    int answer = 0;
+
+    for (int num : nums) {
+        int count = 0;
+
+        while (num > 0) {
+            num = num / 10;
+            count++;
+        }
+
+        if (count % 2 == 0)
+            answer++;
+    }
+
+    return answer;
+}
+
+int main() {
+    vector<int> nums = {12, 345, 2, 6, 7896};
+
+    cout << findNumbers(nums) << endl;
+
+    return 0;
+}
