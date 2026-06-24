6a56398274fad06d853ecd3768d1339ef56e37e4
Shruti Kumari <shrutikumarism226@gmail.com>
2026-06-24 23:52:48 +0530

    Add files via upload

---
+++ Day11.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+
+    ListNode(int x) {
+        val = x;
+        next = NULL;
+    }
+};
+
+ListNode* middleNode(ListNode* head) {
+    ListNode* slow = head;
+    ListNode* fast = head;
+
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    return slow;
+}
+
+int main() {
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    ListNode* middle = middleNode(head);
+
+    cout << "Middle onwards: ";
+    while (middle != NULL) {
+        cout << middle->val << " ";
+        middle = middle->next;
+    }
+
+    return 0;
+}
