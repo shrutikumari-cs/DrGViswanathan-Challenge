d07944d24430537edbb83cdc4797bf2ca90048ab
Shruti Kumari <shrutikumarism226@gmail.com>
2026-06-26 00:12:13 +0530

    Add files via upload

---
+++ Day12.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+bool containsDuplicate(vector<int>& nums) {
+    unordered_set<int> seen;
+
+    for (int num : nums) {
+        if (seen.count(num))
+            return true;
+
+        seen.insert(num);
+    }
+
+    return false;
+}
+
+int main() {
+    int n;
+
+    cout << "Enter n: ";
+    cin >> n;
+
+    vector<int> nums(n);
+
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    if (containsDuplicate(nums))
+        cout << "true";
+    else
+        cout << "false";
+}
